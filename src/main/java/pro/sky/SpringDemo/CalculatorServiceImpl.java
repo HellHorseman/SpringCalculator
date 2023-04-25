87678f752b99dbe635c9ace88279cd4572468fbf
HellHorseman <duke.sheffer@yandex.ru>
2023-04-25 20:22:56 +0400

    retype to interface

---
+++ src/main/java/pro/sky/SpringDemo/CalculatorServiceImpl.java
@@ -0,0 +1,30 @@
+package pro.sky.SpringDemo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+ public class CalculatorServiceImpl implements CalculatorService {
+    public String greetings() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public String sum(int num1, int num2) {
+        int sum = num1 + num2;
+        return num1 + " + " + num2 + " = " + sum;
+    }
+
+    public String diff(int num1, int num2) {
+        int difference = num1 - num2;
+        return num1 + " - " + num2 + " = " + difference;
+    }
+
+    public String mult(int num1, int num2) {
+        int multiple = num1 * num2;
+        return num1 + " * " + num2 + " = " + multiple;
+    }
+
+    public String div(int num1, int num2) {
+        int divide = num1 / num2;
+        return num1 + " / " + num2 + " = " + divide;
+    }
+}
