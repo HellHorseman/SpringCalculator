4b9e144bd26c960b287474f2d879bc1c5b793bca
HellHorseman <duke.sheffer@yandex.ru>
2023-04-25 20:27:05 +0400

    add request mapping

--- src/main/java/pro/sky/SpringDemo/CalculatorController.java
+++ src/main/java/pro/sky/SpringDemo/CalculatorController.java
@@ -1,10 +1,12 @@
 package pro.sky.SpringDemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
     private final pro.sky.SpringDemo.CalculatorService calculatorService;
 
@@ -12,23 +14,23 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
-    @GetMapping(path = "/calculator")
+    @GetMapping
     public String greetings() {
         return calculatorService.greetings();
     }
-    @GetMapping(path = "/calculator/plus")
+    @GetMapping(path = "/plus")
     public String sum(@RequestParam int num1, @RequestParam int num2) {
         return calculatorService.sum(num1, num2);
     }
-    @GetMapping(path = "/calculator/minus")
+    @GetMapping(path = "/minus")
     public String diff(@RequestParam int num1, @RequestParam int num2) {
         return calculatorService.diff(num1, num2);
     }
-    @GetMapping(path = "/calculator/multiple")
+    @GetMapping(path = "/multiple")
     public String mult(@RequestParam int num1, @RequestParam int num2) {
         return calculatorService.mult(num1, num2);
     }
-    @GetMapping(path = "/calculator/divide")
+    @GetMapping(path = "/divide")
     public String div(@RequestParam int num1, @RequestParam int num2) {
         return calculatorService.div(num1, num2);
     }
